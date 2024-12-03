557b56b9bc366c111b6ee336b977330e9a8a214d
Oliver Cullimore <hello@olivercullimore.co.uk>
2024-12-03 16:18:12 +0000

    feat: Instagram Feed auto-update

--- js/main.js
+++ js/main.js
@@ -6,7 +6,6 @@ async function loadInstagramFeed() {
       const response = await fetch("/instagram.html");
       if (!response.ok) throw new Error("Failed to fetch Instagram feed");
       const feed = await response.text();
-      console.log(feed);
       feedElement.innerHTML = feed;
       if (feedElement.innerHTML != "") {
         feedElement.style.display = "grid";
